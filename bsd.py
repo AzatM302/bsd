c90751b0cd13a77f165619fa95b758b14f35b2f5
AzatM302 <zell4e@gmail.com>
2024-07-15 19:00:51 +0300

    Add files via upload

---
+++ bsd.py
@@ -0,0 +1,8 @@
+print(type('1st programm'))
+print((9**0.5)*5)
+print('2nd program')
+print(9.99>9.98 and 1000!=1000.1)
+print('3rd program')
+print(1234%1000//10+5678%5000//10)
+print('4th program')
+print(13.42*100%1300==(int(42.13)))and(42.13*100%4200==(int(13.42)))
